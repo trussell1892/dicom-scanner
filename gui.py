d5f6c7a6b45dba42dc9b4fc3762029df1a727aa0
Tom Russell <tom.russell1892@gmail.com>
2020-03-19 21:02:26 +0000

    cc

--- gui.py
+++ gui.py
@@ -4,8 +4,14 @@ window = Tk()
 
 window.title("Scan DICOMs tool")
 
-btn = Button(text = "Scan DICOMs")
+btn = Button(window, text = "Scan DICOMs")
 btn.grid(column=1, row=1)
+
+lbl = Label(window, text="Patient Name:")
+lbl.grid(column=1, row=2)
+
+txt = Entry(window, width = 25)
+txt.grid(column=2, row=2)
 
 window.geometry("350x250")
 window.mainloop()
