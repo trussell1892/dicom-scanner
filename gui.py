062ad4c566d2c3d942b31b30fef42c9c69580a4e
Tom Russell <tom.russell1892@gmail.com>
2020-03-19 20:48:16 +0000

    c

--- gui.py
+++ gui.py
@@ -2,6 +2,10 @@ from tkinter import *
 
 window = Tk()
 
-window.title("Scan DICOM tool")
+window.title("Scan DICOMs tool")
 
+btn = Button(text = "Scan DICOMs")
+btn.grid(column=1, row=1)
+
+window.geometry("350x250")
 window.mainloop()
